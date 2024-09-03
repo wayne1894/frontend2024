cb05ed87413e1f6616feae38c580109c4bc12e4e
wayne1894 <wayne1894.teach@gmail.com>
2024-09-03 21:36:53 +0800

    no message

--- 0903/mock/index.js
+++ 0903/mock/index.js
@@ -15,6 +15,20 @@ app.get('/', (req, res) => {
     console.log(a,"a")
     res.json({a:1});
 });
+app.get('/load', (req, res) => {
+    res.json([
+        {
+            "custNo": "CUS0000794582",
+            "notifyType": ["LINE", "APP"],
+            "nameChn": "王小明"
+        },
+        {
+            "custNo": "CUS0000794444",
+            "notifyType": ["CUSTOM"],
+            "nameChn": "測試"
+        },
+    ]);
+});
 
 // 啟動伺服器
 app.listen(port, () => {
