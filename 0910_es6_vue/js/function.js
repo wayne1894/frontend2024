7d0027462e68b08f3ba2a8c72237f02fd972e837
wayne1894 <wayne1894.teach@gmail.com>
2024-09-11 00:51:53 +0800

    no message

--- 0910_es6_vue/js/function.js
+++ 0910_es6_vue/js/function.js
@@ -1,24 +1,20 @@
 var name = "王小明";
 
 //一般函式
-function a(){
-    console.log(this.name)
-}
-a();//第一個 (用window來呼叫)
 var teacher = {
     name: "陳緯麟",
-    aa : a
+    aa : function (){
+        console.log(this.name)
+    }
 }
 teacher.aa(); //第二個
 
 //箭頭函式
-let aa = () => {
-    console.log(this.name)
-}
-aa()
 let teacher2 = {
     name: "李三三",
-    aa : aa
+    aa : () => {
+        console.log(this.name)
+    }
 }
 teacher2.aa()
 
