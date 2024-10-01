04d4708dd24233350917bf47f09d522494cc4f15
wayne1894 <wayne1894.teach@gmail.com>
2024-10-01 20:01:11 +0800

    no message

--- 0924/vue-project/src/views/AboutView.vue
+++ 0924/vue-project/src/views/AboutView.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h2>dddddd</h2>
-  </div>
+  <main>
+    <Header></Header>
+    <hr>
+    這是About
+  </main>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script>
+import Header from '../components/Header.vue'
+
+export default{
+  components:{
+    Header,
   }
 }
-</style>
+</script>
--- 0924/vue-project/src/views/DemoView.vue
+++ 0924/vue-project/src/views/DemoView.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1 class="text-[red]">有是 demo 的 h1</h1>
-    <p class="bg-[green]">我是 p 標籤</p>
-    <div>ddddd</div>
-</template>
-<script>
-    export default {
-
+    <main>
+      <Header></Header>
+      <hr>
+      <h1 class="text-[red]">有是 demo 的 h1</h1>
+      <p class="bg-[green]">我是 p 標籤</p>
+      <div>ddddd</div>
+    </main>
+  </template>
+  
+  <script>
+  import Header from '../components/Header.vue'
+  
+  export default{
+    components:{
+      Header,
     }
-</script>
+  }
+  </script>
+  
+
